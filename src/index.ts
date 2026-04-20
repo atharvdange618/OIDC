17bf5c97cb47becd70af20de63185ecef9b6fd61
Atharv Dange <atharvdange618@gmail.com>
2026-04-21 02:26:53 +0530

    feat: add AppError classes, discovery router, and update tsconfig include

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import dotenv from "dotenv";
+import discoveryRouter from "./routes/discovery.routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
 
@@ -12,6 +14,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/.well-known", discoveryRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`IdP running on http://localhost:${PORT}`);
