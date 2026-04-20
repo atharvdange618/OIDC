34f935428882a0ba14f4dd58f2e7a07587394654
Atharv Dange <atharvdange618@gmail.com>
2026-04-21 02:22:00 +0530

    refactor: refactor codebase to use routes-controller-service pattern

---
+++ src/routes/discovery.routes.ts
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from "express";
+import { getJwks } from "../lib/jwks";
+import { ISSUER } from "../config/keys";
+
+const router = Router();
+
+router.get("/openid-configuration", async (req: Request, res: Response) => {
+  res.json({
+    issuer: ISSUER,
+    authorization_endpoint: `${ISSUER}/authorize`,
+    token_endpoint: `${ISSUER}/token`,
+    userinfo_endpoint: `${ISSUER}/userinfo`,
+    jwks_uri: `${ISSUER}/.well-known/jwks.json`,
+    scopes_supported: ["openid", "profile", "email"],
+    response_types_supported: ["code"],
+    grant_types_supported: ["authorization_code"],
+    subject_types_supported: ["public"],
+    id_token_signing_alg_values_supported: ["RS256"],
+    code_challenge_methods_supported: ["S256"],
+  });
+});
+
+router.get("/jwks.json", async (req: Request, res: Response) => {
+  const jwks = await getJwks();
+  res.json(jwks);
+});
+
+export default router;
