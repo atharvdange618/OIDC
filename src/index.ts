da1e4e3f58ddd9f14f4d38be7310f1bc57e4e754
Atharv Dange <atharvdange618@gmail.com>
2026-04-21 18:57:00 +0530

    feat: add authorization route and hpp middleware

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,26 @@ dotenv.config();
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import hpp from "hpp";
 import { errorHandler } from "./middleware/errorHandler";
 import discoveryRouter from "./routes/discovery.routes";
 import authRouter from "./routes/auth.routes";
 import clientsRouter from "./routes/clients.routes";
+import authorizeRouter from "./routes/authorize.routes";
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(helmet());
 app.use(cors());
+app.use(hpp());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/.well-known", discoveryRouter);
 app.use("/auth", authRouter);
 app.use("/clients", clientsRouter);
+app.use("/authorize", authorizeRouter);
 
 app.use(errorHandler);
 
